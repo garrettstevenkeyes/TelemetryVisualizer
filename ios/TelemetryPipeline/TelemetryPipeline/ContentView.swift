f3106000e78d46a6a7c026e475bf566572e4125c
Garrett Keyes <garrett.keyes@gmail.com>
2025-12-29 20:22:05 -0500

    Add iOS app to main repo (remove nested git repo)

---
+++ ios/TelemetryPipeline/TelemetryPipeline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TelemetryPipeline
+//
+//  Created by Garrett Keyes on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/TelemetryPipeline/TelemetryPipeline/TelemetryPipelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelemetryPipelineApp.swift
+//  TelemetryPipeline
+//
+//  Created by Garrett Keyes on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TelemetryPipelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
