1003c97eb38556201d7b82758f585248e4a3a0db
Garrett Keyes <garrett.keyes@gmail.com>
2025-12-30 19:54:25 -0500

    Add assets to the project

---
+++ ios/TelemetryPipeline/TelemetryPipeline/MetricView.swift
@@ -0,0 +1,18 @@
+//
+//  MetricView.swift
+//  TelemetryPipeline
+//
+//  Created by Garrett Keyes on 12/29/25.
+//
+
+import SwiftUI
+
+struct MetricView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MetricView()
+}
